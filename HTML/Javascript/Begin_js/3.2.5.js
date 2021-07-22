7868a7f2d88a9ea0963b1beef6656eaecc264d2d
김경호 <nestjs01@naver.com>
2021-07-22 17:02:57 +0900

    delete_js

---
+++ HTML/Javascript/Begin_js/3.2.5.js
@@ -0,0 +1,10 @@
+const foo = {
+    name: 'foo',
+    nickname: 'babo'
+};
+console.log(foo.nickname);
+delete foo.nickname;// foo 객체의 nickname 프로퍼티를 삭제
+console.log(foo.nickname);
+
+delete foo;
+console.log(foo); // 객체는 삭제할 수 없다.
