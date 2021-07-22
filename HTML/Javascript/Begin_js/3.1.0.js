d64f6c2b4e615bc81018269cb90d041b6eadf96a
김경호 <nestjs01@naver.com>
2021-07-22 17:00:11 +0900

    NULL&Undefinded_js

---
+++ HTML/Javascript/Begin_js/3.1.0.js
@@ -0,0 +1,23 @@
+const intNum = 10; // Number
+const floatNum = 0.1;
+
+const singleQuoteStr = 'single quote string'; //String
+const doubleQuoteSte = "double quote string";
+const singleChar = 'a';
+
+const boolVar = true; //Boolean
+
+let emptyVar; //Undefined
+
+const nullVar = null; //NULL
+
+console.log(
+    typeof intNum,
+    typeof floatNum,
+    typeof singleQuoteStr,
+    typeof doubleQuoteSte,
+    typeof singleChar,
+    typeof boolVar,
+    typeof emptyVar,
+    typeof nullVar
+)
