76459c2fb94bf8df91c2012e8500a64ac99faa6a
김경호 <nestjs01@naver.com>
2021-07-23 16:33:05 +0900

    Array1_js

---
+++ HTML/Javascript/Begin_js/3.5.3.0.js
@@ -0,0 +1,11 @@
+const arr = [];
+console.log(arr, length);
+
+arr[0] = 0;
+console.log(arr.length);
+arr[1] = 1;
+console.log(arr.length);
+arr[2] = 2;
+console.log(arr.length);
+arr[100] = 100;
+console.log(arr.length);
