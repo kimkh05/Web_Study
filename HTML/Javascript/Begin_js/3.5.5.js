1ed2a3f9f2d52911cf47573a29e56c5b834c2d5b
김경호 <nestjs01@naver.com>
2021-07-23 16:33:58 +0900

    Array-length1_js

---
+++ HTML/Javascript/Begin_js/3.5.5.js
@@ -0,0 +1,11 @@
+const arr = ['zero', 'one', 'two'];
+
+arr.color = 'blue';
+arr.name = 'number_array';
+console.log(arr);
+console.log(arr.length);
+
+arr[3] = 'red';
+console.log(arr.length);
+
+console.log(arr);
