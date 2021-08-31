814a0c221d2dc6961638e8ca44a77bce63a5faf3
김경호 <nestjs01@naver.com>
2021-08-31 14:33:39 +0900

    .

---
+++ Frontend/School_DMS/app.js
@@ -0,0 +1,14 @@
+var date = document.getElementById('date');
+function getCurrentDate()
+    {
+        var date = new Date();
+        var year = date.getFullYear().toString();
+
+        var month = date.getMonth() + 1;
+        month = month < 10 ? '0' + month.toString() : month.toString();
+
+        var day = date.getDate();
+        day = day < 10 ? '0' + day.toString() : day.toString();
+
+        return year + month + day ;
+    }
